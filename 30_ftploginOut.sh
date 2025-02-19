277c48b1a24e414f11142123708119aa33c909ef
fnattkemper <nattkemper.federico@gmail.com>
2025-02-19 18:02:32 -0300

    Descripción de los cambios en el submódulo

--- 30_ftploginOut.sh
+++ 30_ftploginOut.sh
@@ -4,10 +4,9 @@
 #Detalles del servidor FTP
 SFTP_SERVER="us-east-1.sftpcloud.io"
 SFTP_USER="fedenatt"
-SFTP_PASS="{$SFTP_PASS}"
+SFTP_PASS="{$SFTP_PASS}" #PW OCULTADA
 
 #Log into the SFTP server
 echo "ls" | sftp -oport=22  $SFTP_USER@$SFTP_SERVER
 
 echo "FTP SESION COMPLETADA"
-
